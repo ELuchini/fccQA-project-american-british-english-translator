0aea233114aef3bc4fe3c8dfee91cf245a77bdcc
Eluchini <eluchini@outlook.com>
2025-09-02 16:12:03 -0300

    Starting up

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,12 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+  app.route("/api/translate").post((req, res) => {
+    console.log(`Res: ${JSON.stringify(req.body)}`);
+    res.json({ translation: "Hello World" });
+  });
 };
