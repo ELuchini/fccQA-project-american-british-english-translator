e462d1ed2c0506a85c2fc1d43810497d1e114267
Eluchini <eluchini@outlook.com>
2025-09-03 08:41:32 -0300

    feat: implement translation functionality and validation in Translator class

--- components/translator.js
+++ components/translator.js
@@ -1,8 +1,100 @@
-const americanOnly = require("./american-only.js");
-const americanToBritishSpelling = require("./american-to-british-spelling.js");
-const americanToBritishTitles = require("./american-to-british-titles.js");
-const britishOnly = require("./british-only.js");
+class Translator {
+  constructor(americanOnly, amToBrSpelling, amToBrTitles, britishOnly) {
+    this.americanOnlyK = Object.keys(americanOnly);
+    this.americanOnlyV = Object.values(americanOnly);
+    this.amToBrSpellingK = Object.keys(amToBrSpelling);
+    this.amToBrSpellingV = Object.values(amToBrSpelling);
+    this.amToBrTitlesK = Object.keys(amToBrTitles);
+    this.amToBrTitlesV = Object.values(amToBrTitles);
+    this.britishOnlyK = Object.keys(britishOnly);
+    this.britishOnlyV = Object.values(britishOnly);
+    this.spanOpen = '<span class="highlight">';
+    this.spanClose = "</span>";
+    this.isCap = false;
+  }
+  translate(text, locale) {
+    // console.log("locale: " + locale);
+    // console.log("text: " + text);
+    // console.log(this.amToBrSpellingK.length);
 
-class Translator {}
+    switch (locale) {
+      case "american-to-british":
+        // console.log("case: american-to-british");
+        return this.americanToBritish(text);
+      case "british-to-american":
+        // console.log("case: british-to-american");
+        return this.britishToAmerican(text);
+      default:
+        return text;
+    }
+  }
+
+  americanToBritish(text) {
+    for (let i = 0; i < this.americanOnlyK.length; i++) {
+      text = text.replace(
+        this.americanOnlyK[i],
+        `${this.spanOpen}${this.americanOnlyV[i]}${this.spanClose}`
+      );
+    }
+    for (let i = 0; i < this.amToBrSpellingK.length; i++) {
+      text = text.replace(
+        this.amToBrSpellingK[i],
+        `${this.spanOpen}${this.amToBrSpellingV[i]}${this.spanClose}`
+      );
+    }
+    for (let i = 0; i < this.amToBrTitlesK.length; i++) {
+      text = text.replace(
+        this.amToBrTitlesK[i],
+        `${this.spanOpen} ${this.amToBrTitlesV[i]} ${this.spanClose}`
+      );
+    }
+    return text;
+  }
+
+  britishToAmerican(text) {
+    for (let i = 0; i < this.britishOnlyK.length; i++) {
+      text = text.replace(
+        this.britishOnlyK[i],
+        `${this.spanOpen} ${this.britishOnlyV[i]} ${this.spanClose}`
+      );
+    }
+    for (let i = 0; i < this.amToBrSpellingK.length; i++) {
+      text = text.replace(
+        this.amToBrSpellingV[i],
+        `${this.spanOpen} ${this.amToBrSpellingK[i]} ${this.spanClose}`
+      );
+    }
+    for (let i = 0; i < this.amToBrTitlesK.length; i++) {
+      // if (text[0] == text[0].toUpperCase()) {
+      //   this.isCap = true;
+      // }
+      //   if (text.toLowerCase().includes(this.amToBrTitlesV[i])) {
+      text = text.replace(
+        this.amToBrTitlesV[i],
+        `${this.spanOpen} ${this.amToBrTitlesK[i]} ${this.spanClose}`
+      );
+    }
+    // }
+    return text;
+  }
+
+  validate(text, locale) {
+    if (locale !== "american-to-british" && locale !== "british-to-american") {
+      return { error: "Invalid value for locale field" };
+    }
+    if (text == "") {
+      console.log("No text to translate");
+      return { error: "No text to translate" };
+    }
+    if (!text || !locale) {
+      console.log("Required field(s) missing");
+      return { error: "Required field(s) missing" };
+    }
+    if (typeof text !== "string") {
+      console.log("Text is not a string, No text to translate");
+      return { error: "No text to translate" };
+    }
+  }
+}
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,36 @@
 "use strict";
 
 const Translator = require("../components/translator.js");
+const americanOnly = require("../components/american-only.js");
+const americanToBritishSpelling = require("../components/american-to-british-spelling.js");
+const americanToBritishTitles = require("../components/american-to-british-titles.js");
+const britishOnly = require("../components/british-only.js");
+const translator = new Translator(
+  americanOnly,
+  americanToBritishSpelling,
+  americanToBritishTitles,
+  britishOnly
+);
 
 module.exports = function (app) {
-  const translator = new Translator();
-
   app.route("/api/translate").post((req, res) => {
-    console.log(`Res: ${JSON.stringify(req.body)}`);
-    res.json({ translation: "Hello World" });
+    console.log(`[API IN]: ${JSON.stringify(req.body)}`);
+    const validation = translator.validate(req.body.text, req.body.locale);
+    if (validation) {
+      res.json(validation);
+      return;
+    }
+
+    let translation = translator.translate(req.body.text, req.body.locale);
+    if (translation == req.body.text) {
+      console.log("Everything looks good to me!");
+      translation = "Everything looks good to me!";
+    }
+    const response = {
+      text: req.body.text,
+      translation: translation,
+    };
+    console.log(`[API OU]: ${JSON.stringify(response)}`);
+    res.json(response);
   });
 };
